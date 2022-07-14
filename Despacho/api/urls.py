1e1b641eb1d26a7f75e9ae812618b3059d66cf2d
Saxso7 <saxso1991@outlook.com>
2022-07-14 02:17:42 -0400

    avance

--- Despacho/api/urls.py
+++ Despacho/api/urls.py
@@ -3,10 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('envioGet',views.EnvioGet.as_view(), name='ver envio'),
-    path('envioGet/<int:nroSeguimiento>',views.EnvioGet.as_view(),name='ver envio<rut>'),
-    path('envioPost',views.EnvioVistaPost.as_view(), name='crear envio'),
-    path('envioDelete/<int:nroSeguimiento>',views.EliminarEnvio.as_view(),name='eliminar envio <id>'),
+    path('lista_envio',views.envio, name='ver envio'),
+    path('lista_envio/<int:nroSeguimiento>',views.envio,name='ver envio<rut>'),
+    path('lista_envio/<int:nroSeguimiento>',views.envio,name='eliminar envio <id>'),
     path('lista_entrega',views.entrega, name='lista_entrega'),
     path('lista_entrega/<int:nroSeguimiento>',views.entrega, name='lista_entrega'),
     path('lista_entrega/<int:nroSeguimiento>',views.entrega,name='eliminar entrega <id>')
--- Despacho/api/views.py
+++ Despacho/api/views.py
@@ -12,20 +12,18 @@ from .serializers import EnvioSerializers, EntregaSerializers
 
 # Create your views here.
 
-class EnvioGet(APIView):
-    serializer_class = EnvioSerializers
-    
-    def get(self,request, nroSeguimiento=0):
+@csrf_exempt
+@api_view(['GET','POST','DELETE'])   
+def envio(request, nroSeguimiento=0):
+    if request.method == 'GET':
         if(nroSeguimiento > 0):
             envio=Envio.objects.filter(nroSeguimiento=nroSeguimiento).values()
             if len(envio) > 0:
-                envios = envio[0]
-                datos = {'envio':envios}
+                envio = envio[0]
+                datos = {'envio':envio}
             else:
                 datos={'message':'nroSeguimiento no encontrado'}
             return JsonResponse(datos)
-                
-            
         else:    
             envio=Envio.objects.all()
             if len(envio)>0:
@@ -33,28 +31,15 @@ class EnvioGet(APIView):
             else:
                 datos={'message':'not found'}
             serializer=EnvioSerializers(envio,many=True)  
-            return Response(serializer.data)   
- 
-      
-class EnvioVistaPost(APIView):  
-    serializer_class = EnvioSerializers
-        
-    def post(self, request, *args, **kwargs):
-        envioCrear = request.data
-        print(envioCrear)
-        envioCreado = Envio.objects.create(nombre=envioCrear['nombre'],rut=envioCrear['rut'],direccion=envioCrear['direccion'],
-                             producto=envioCrear['producto'],
-                             cantidad=envioCrear['cantidad'],precio=envioCrear['precio'])
-        
-        envioCreado.save()
-        
-        serializer = EnvioSerializers(envioCreado)
-        
-        return Response(serializer.data)
-
-class EliminarEnvio(APIView):
-    
-    def delete(self, request, nroSeguimiento):
+            return Response(serializer.data)
+    elif request.method == 'POST':
+        serializers = EnvioSerializers(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
         eliminarEnvio = Envio.objects.get(nroSeguimiento=nroSeguimiento)
         eliminarEnvio.delete()
         datos = {'message':'eliminado con exito'}
